74c906c8f76c51f987ae1be6e6b61d8a0d172591
Evgeniy Muravetskiy <Ekiruui@gmail.com>
2023-03-22 21:16:59 +0300

    fix: statclient

--- stats/stats-client/src/main/java/ru/practicum/statclient/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/statclient/StatClient.java
@@ -1,6 +1,7 @@
 package ru.practicum.statclient;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -15,19 +16,22 @@ public class StatClient {
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private final WebClient webClient;
 
+    @Value("${app.name}")
+    private String appName;
+
     public StatClient(String url) {
         this.webClient = WebClient.create(url);
     }
 
     public void addHit(HttpServletRequest httpRequest) {
         EndpointHitDto endpointHitDto = EndpointHitDto.builder()
-                .app("ewm-service")
+                .app(appName)
                 .ip(httpRequest.getRemoteAddr())
                 .uri(httpRequest.getRequestURI())
                 .timestamp(LocalDateTime.now().format(DATE_TIME_FORMATTER))
                 .build();
-        log.info("IP:" + endpointHitDto.getIp());
-        log.info("URI:" + endpointHitDto.getUri());
+        log.info("IP: {}", endpointHitDto.getIp());
+        log.info("URI: {}", endpointHitDto.getUri());
         webClient.post()
                 .uri("/hit")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
