3a0375bcbe2ab1ba3a70d1c412e8344f55cd6ca0
Evgeniy Muravetskiy <Ekiruui@gmail.com>
2023-03-22 21:44:57 +0300

    fix: imports

--- stats/stats-client/src/main/java/ru/practicum/statclient/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/statclient/StatClient.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.practicum.statdto.EndpointHitDto;
 
